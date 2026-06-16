94536ba5b8054a588767273a5a46a0666049f0a9
chalicha <112318025+chalicha@users.noreply.github.com>
2026-06-16 17:29:40 +0400

    Create index.js

---
+++ src/lampac/index.js
@@ -0,0 +1,29 @@
+import { createApi } from './api.js';
+
+var BASE_URL = 'https://lam.maxvol.pro'; // შეცვალე საკუთარი სერვერით თუ გაქვს
+
+async function getStreams(tmdbId, mediaType, season, episode) {
+    console.log('[Lampac] Request:', mediaType, tmdbId);
+    var api = createApi(BASE_URL);
+    try {
+        var balancers = await api.fetchBalancers(tmdbId, mediaType);
+        if (!balancers || balancers.length === 0) return [];
+        
+        var allStreams = [];
+        for (var i = 0; i < balancers.length; i++) {
+            var b = balancers[i];
+            if (!b.show) continue;
+            try {
+                var streams = await api.fetchFromBalancer(b.url, tmdbId, mediaType, season, episode);
+                streams.forEach(function(s) { s.name = b.name + (s.voiceName ? ' · ' + s.voiceName : ''); });
+                allStreams = allStreams.concat(streams);
+            } catch(e) { console.log('[Lampac] Balancer error:', e.message); }
+        }
+        return allStreams;
+    } catch(e) {
+        console.error('[Lampac] Error:', e.message);
+        return [];
+    }
+}
+
+module.exports = { getStreams };
