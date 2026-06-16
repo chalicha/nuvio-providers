141ec14e1cb980758d5fc8b7353721049c59ee52
chalicha <112318025+chalicha@users.noreply.github.com>
2026-06-16 16:15:21 +0400

    Create BwaRC.js

---
+++ providers/BwaRC.js
@@ -0,0 +1,14 @@
+function getStreams(tmdbId, mediaType, season, episode) {
+  console.log("[BWARC] test", tmdbId);
+
+  return Promise.resolve([
+    {
+      name: "BWARC Test",
+      title: "Working Test 1080p",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+      quality: "1080p"
+    }
+  ]);
+}
+
+module.exports = { getStreams };
